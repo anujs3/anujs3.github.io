40a61696ec5b1be3fee7431e078647b641a7bc46
Anuj Shah <Anuj Shah>
2016-09-06 15:22:16 -0700

    Updated webpage.

--- email_handler.php
+++ email_handler.php
@@ -3,44 +3,17 @@
 $first_name = ucfirst($_POST['first_name']);
 $last_name = ucfirst($_POST['last_name']);
 $email_address = $_POST['email'];
-$number_attendees = $_POST['num_attendees'];
-$names = $_POST['names'];
 $message = $_POST['message'];
 
-$to = "aashiparth@gmail.com";
-$subject = "New RSVP";
+$to = "anujshah26@gmail.com";
+$subject = "New Message";
 $headers = "From: $email_address\r\n";
 
-$choice = "No";
-if ($_POST['choice'] == "yes")
-{
-	$choice = "Yes";
-}
-
-$team = "No Team";
-if ($_POST['team'] == "Aashita")
-{
-	$team = "Team Aashita";
-}
-else if ($_POST['team'] == "Parth")
-{
-	$team = "Team Parth";
-}
-
-$message = "A visitor to your site has sent the following RSVP information:\n\nFirst Name: $first_name\nLast Name: $last_name\n\nEmail Address: $email_address\nAttending? $choice\n\nNumber of Attendees: $number_attendees\nAttendee Names: $names\n\n$team\n\nMessage:\n$message";
+$message = "A visitor to your site has sent the following information:\n\nFirst Name: $first_name\nLast Name: $last_name\n\nEmail Address: $email_address\nMessage:\n$message";
 
 if (mail($to,$subject,$message,$headers))
 {
-	header('Location: AashitaParth.html#RSVP');
-	
-	/*
-	echo "
-            <script type=\"text/javascript\">
-            document.getElementById(\"formSubmitted\").innerHTML = \"RSVP Successfully Submitted!\";
-            </script>
-         ";
-	*/
-	// echo "SUCCESS";
+	header('Location: AnujShah.html#Home');
 }
 else
 {
